d59b710f2ec74b40c3beddba1ec39f4172a5ed62
Gregory Arcentales <gregoarcenta@gmail.com>
2024-12-30 11:23:12 -0500

    delete testModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
-import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [MessagesModule, TestModule],
+  imports: [MessagesModule],
   controllers: [],
   providers: [],
 })
--- src/test/dto/create-test.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, IsPositive } from 'class-validator';
-import { Type } from 'class-transformer';
-
-export class CreateTestDto {
-  @IsNumber()
-  @IsPositive()
-  @Type(() => Number)
-  message: string;
-}
